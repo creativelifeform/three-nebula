3f4c6cc9c31b5eac2a5e400dee125281a7d1ce00
Rohan Deshpande <rohan@creativelifeform.com>
2018-11-22 06:33:03 +1100

    Ported core/Pool and utils/PUID to es6

--- src/core/Pool.js
+++ src/core/Pool.js
@@ -1,57 +1,52 @@
-(function(Proton, undefined) {
+import PUID from '../utils/PUID';
 
-    function Pool() {
-        this.cID = 0;
-        this.list = {};
+export default class Pool {
+  constructor() {
+    this.cID = 0;
+    this.list = {};
+  }
+
+  create(obj) {
+    this.cID++;
+
+    if (typeof obj == 'function') return new obj();
+    else return obj.clone();
+  }
+
+  getCount() {
+    var count = 0;
+    for (var id in this.list) count += this.list[id].length;
+
+    return count++;
+  }
+
+  get(obj) {
+    var p,
+      puid = obj.__puid || PUID.id(obj);
+    if (this.list[puid] && this.list[puid].length > 0)
+      p = this.list[puid].pop();
+    else p = this.create(obj);
+
+    p.__puid = obj.__puid || puid;
+
+    return p;
+  }
+
+  expire(obj) {
+    return this._getList(obj.__puid).push(obj);
+  }
+
+  destroy() {
+    for (var id in this.list) {
+      this.list[id].length = 0;
+      delete this.list[id];
     }
+  }
 
-    Pool.prototype = {
-        create: function(obj) {
-            this.cID++;
+  _getList(uid) {
+    uid = uid || 'default';
+    if (!this.list[uid]) this.list[uid] = [];
 
-            if (typeof obj == "function")
-                return new obj;
-            else
-                return obj.clone();
-        },
-
-        getCount: function() {
-            var count = 0;
-            for (var id in this.list)
-                count += this.list[id].length;
-
-            return count++;;
-        },
-
-        get: function(obj) {
-            var p, puid = obj.__puid || Proton.PUID.id(obj);
-            if (this.list[puid] && this.list[puid].length > 0)
-                p = this.list[puid].pop();
-            else
-                p = this.create(obj);
-
-            p.__puid = obj.__puid || puid;
-            return p;
-        },
-
-        expire: function(obj) {
-            return this._getList(obj.__puid).push(obj);
-        },
-
-        destroy: function() {
-            for (var id in this.list) {
-                this.list[id].length = 0;
-                delete this.list[id];
-            }
-        },
-
-        _getList: function(uid) {
-            uid = uid || "default";
-            if (!this.list[uid]) this.list[uid] = [];
-            return this.list[uid];
-        }
-    }
-
-    Proton.Pool = Pool;
-
-})(Proton);
+    return this.list[uid];
+  }
+}
