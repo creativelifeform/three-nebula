0fd223abd8b05d92ca0cdff01d9c91c8c7143fda
rohandeshpande <rohandeshpande@nbnco.com.au>
2018-11-22 11:03:01 +1100

    Ported remaining emitters and THREEUtil to es6

--- src/emitter/FollowEmitter.js
+++ src/emitter/FollowEmitter.js
@@ -1,86 +1,97 @@
-(function(Proton, undefined) {
-    /**
-     * The FollowEmitter class inherits from Proton.Emitter
-     *
-     * use the FollowEmitter will emit particle when mousemoving
-     *
-     * @class Proton.FollowEmitter
-     * @constructor
-     * @param {Element} mouseTarget mouseevent's target;
-     * @param {Number} ease the easing of following speed;
-     * @default 0.7
-     * @param {Object} pObj the parameters object;
-     */
-    function FollowEmitter(mouseTarget, ease, pObj) {
-        this.mouseTarget = Proton.Util.initValue(mouseTarget, window);
-        this.ease = Proton.Util.initValue(ease, .7);
-        this._allowEmitting = false;
-        this.initEventHandler();
-        FollowEmitter._super_.call(this, pObj);
+import Emitter from './Emitter';
+import THREEUtil from '../utils/THREEUtil';
+import Util from '../utils/Util';
+
+export default class FollowEmitter extends Emitter {
+  /**
+   * The FollowEmitter class inherits from Proton.Emitter
+   *
+   * use the FollowEmitter will emit particle when mousemoving
+   *
+   * @class Proton.FollowEmitter
+   * @constructor
+   * @param {Element} mouseTarget mouseevent's target;
+   * @param {Number} ease the easing of following speed;
+   * @default 0.7
+   * @param {Object} pObj the parameters object;
+   */
+  constructor(mouseTarget, ease, pObj) {
+    super(pObj);
+
+    this.mouseTarget = Util.initValue(mouseTarget, window);
+    this.ease = Util.initValue(ease, 0.7);
+    this._allowEmitting = false;
+    this.initEventHandler();
+  }
+
+  initEventHandler() {
+    var self = this;
+
+    this.mousemoveHandler = function(e) {
+      self.mousemove.call(self, e);
     };
 
-    Proton.Util.inherits(FollowEmitter, Proton.Emitter);
-    FollowEmitter.prototype.initEventHandler = function() {
-        var self = this;
-        this.mousemoveHandler = function(e) {
-            self.mousemove.call(self, e);
-        };
-
-        this.mousedownHandler = function(e) {
-            self.mousedown.call(self, e);
-        };
-
-        this.mouseupHandler = function(e) {
-            self.mouseup.call(self, e);
-        };
-        
-        this.mouseTarget.addEventListener('mousemove', this.mousemoveHandler, false);
-    }
-
-    /**
-     * start emit particle
-     * @method emit
-     */
-    FollowEmitter.prototype.emit = function() {
-        this._allowEmitting = true;
-    }
-
-    /**
-     * stop emiting
-     * @method stopEmit
-     */
-    FollowEmitter.prototype.stopEmit = function() {
-        this._allowEmitting = false;
-    }
-
-    FollowEmitter.prototype.setCameraAndCanvas = function(camera, canvas) {
-        this.camera = camera;
-        this.canvas = canvas;
-    }
-
-    FollowEmitter.prototype.mousemove = function(e) {
-        if (e.layerX || e.layerX == 0) {
-            this.p.x += (e.layerX - this.p.x) * this.ease;
-            this.p.y += (e.layerY - this.p.y) * this.ease;
-        } else if (e.offsetX || e.offsetX == 0) {
-            this.p.x += (e.offsetX - this.p.x) * this.ease;
-            this.p.y += (e.offsetY - this.p.y) * this.ease;
-        }
-
-        this.p.copy(Proton.THREEUtil.toSpacePos(this.p, this.camera, this.canvas));
-
-        if (this._allowEmitting)
-            FollowEmitter._super_.prototype.emit.call(this, 'once');
+    this.mousedownHandler = function(e) {
+      self.mousedown.call(self, e);
     };
 
-    /**
-     * Destory this Emitter
-     * @method destroy
-     */
-    FollowEmitter.prototype.destroy = function() {
-        FollowEmitter._super_.prototype.destroy.call(this);
-        this.mouseTarget.removeEventListener('mousemove', this.mousemoveHandler, false);
+    this.mouseupHandler = function(e) {
+      self.mouseup.call(self, e);
+    };
+
+    this.mouseTarget.addEventListener(
+      'mousemove',
+      this.mousemoveHandler,
+      false
+    );
+  }
+
+  /**
+   * start emit particle
+   * @method emit
+   */
+  emit() {
+    this._allowEmitting = true;
+  }
+
+  /**
+   * stop emiting
+   * @method stopEmit
+   */
+  stopEmit() {
+    this._allowEmitting = false;
+  }
+
+  setCameraAndCanvas(camera, canvas) {
+    this.camera = camera;
+    this.canvas = canvas;
+  }
+
+  mousemove(e) {
+    if (e.layerX || e.layerX == 0) {
+      this.p.x += (e.layerX - this.p.x) * this.ease;
+      this.p.y += (e.layerY - this.p.y) * this.ease;
+    } else if (e.offsetX || e.offsetX == 0) {
+      this.p.x += (e.offsetX - this.p.x) * this.ease;
+      this.p.y += (e.offsetY - this.p.y) * this.ease;
     }
 
-    Proton.FollowEmitter = FollowEmitter;
-})(Proton);
+    this.p.copy(THREEUtil.toSpacePos(this.p, this.camera, this.canvas));
+
+    if (this._allowEmitting)
+      FollowEmitter._super_.prototype.emit.call(this, 'once');
+  }
+
+  /**
+   * Destory this Emitter
+   * @method destroy
+   */
+  destroy() {
+    FollowEmitter._super_.prototype.destroy.call(this);
+    this.mouseTarget.removeEventListener(
+      'mousemove',
+      this.mousemoveHandler,
+      false
+    );
+  }
+}
--- src/utils/THREEUtil.js
+++ src/utils/THREEUtil.js
@@ -1,62 +1,67 @@
 import * as THREE from 'three';
 
-(function(Proton, undefined) {
-  var THREEUtil = {
-    toScreenPos: (function() {
-      var vector = new THREE.Vector3();
+import PUID from './PUID';
 
-      return function(pos, camera, canvas) {
-        vector.copy(pos);
-        // map to normalized device coordinate (NDC) space
-        vector.project(camera);
-        // map to 2D screen space
-        vector.x = Math.round(((vector.x + 1) * canvas.width) / 2);
-        vector.y = Math.round(((-vector.y + 1) * canvas.height) / 2);
-        vector.z = 0;
+export default {
+  toScreenPos: (function() {
+    var vector = new THREE.Vector3();
 
-        return vector;
-      };
-    })(),
+    return function(pos, camera, canvas) {
+      vector.copy(pos);
+      // map to normalized device coordinate (NDC) space
+      vector.project(camera);
+      // map to 2D screen space
+      vector.x = Math.round(((vector.x + 1) * canvas.width) / 2);
+      vector.y = Math.round(((-vector.y + 1) * canvas.height) / 2);
+      vector.z = 0;
 
-    toSpacePos: (function() {
-      var vector = new THREE.Vector3(),
-        dir = new THREE.Vector3(),
-        distance;
+      return vector;
+    };
+  })(),
 
-      return function(pos, camera, canvas) {
-        vector.set(
-          (pos.x / canvas.width) * 2 - 1,
-          -(pos.y / canvas.height) * 2 + 1,
-          0.5
-        );
-        vector.unproject(camera);
+  toSpacePos: (function() {
+    var vector = new THREE.Vector3(),
+      dir = new THREE.Vector3(),
+      distance;
 
-        dir.copy(vector.sub(camera.position).normalize());
-        distance = -camera.position.z / dir.z;
-        vector.copy(camera.position);
-        vector.add(dir.multiplyScalar(distance));
-        return vector;
-      };
-    })(),
+    return function(pos, camera, canvas) {
+      vector.set(
+        (pos.x / canvas.width) * 2 - 1,
+        -(pos.y / canvas.height) * 2 + 1,
+        0.5
+      );
+      vector.unproject(camera);
 
-    getTexture: (function() {
-      var store = {};
+      dir.copy(vector.sub(camera.position).normalize());
+      distance = -camera.position.z / dir.z;
+      vector.copy(camera.position);
+      vector.add(dir.multiplyScalar(distance));
 
-      return function(img) {
-        if (img instanceof THREE.Texture) {
-          return img;
-        } else if (typeof img == 'string') {
-          var id = Proton.PUID.hash(img);
-          if (!store[id]) store[id] = new THREE.Texture(img);
-          return store[id];
-        } else if (img instanceof Image) {
-          var id = Proton.PUID.hash(img.src);
-          if (!store[id]) store[id] = new THREE.Texture(img);
-          return store[id];
-        }
-      };
-    })()
-  };
+      return vector;
+    };
+  })(),
 
-  Proton.THREEUtil = THREEUtil;
-})(Proton);
+  getTexture: (function() {
+    var store = {};
+
+    return function(img) {
+      let id;
+
+      if (img instanceof THREE.Texture) {
+        return img;
+      } else if (typeof img == 'string') {
+        id = PUID.hash(img);
+
+        if (!store[id]) store[id] = new THREE.Texture(img);
+
+        return store[id];
+      } else if (img instanceof Image) {
+        id = PUID.hash(img.src);
+
+        if (!store[id]) store[id] = new THREE.Texture(img);
+
+        return store[id];
+      }
+    };
+  })()
+};
