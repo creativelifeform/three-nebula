65ae9cddd43e61808bd76794ce3f03647beb4ec6
rohandeshpande <rohandeshpande@nbnco.com.au>
2018-11-22 13:21:13 +1100

    A little bit of weirdness with applyEuler and applyAxisAngle methods in the Vector3D class, I think what I've done sorts it for now

--- src/math/Vector3D.js
+++ src/math/Vector3D.js
@@ -1,5 +1,6 @@
 //@author mrdoob / http://mrdoob.com/
 
+import Quaternion from './Quaternion';
 import { Vector3 } from 'three';
 
 export default class Vector3D extends Vector3 {
@@ -15,3 +16,25 @@ export default class Vector3D extends Vector3 {
     return 'x:' + this.x + 'y:' + this.y + 'z:' + this.z;
   }
 }
+
+Vector3D.prototype.applyEuler = (function() {
+  let quaternion;
+
+  return function applyEuler(euler) {
+    if (quaternion === undefined) quaternion = new Quaternion();
+    this.applyQuaternion(quaternion.setFromEuler(euler));
+
+    return this;
+  };
+})();
+
+Vector3D.prototype.applyAxisAngle = (function() {
+  let quaternion;
+
+  return function applyAxisAngle(axis, angle) {
+    if (quaternion === undefined) quaternion = new Quaternion();
+    this.applyQuaternion(quaternion.setFromAxisAngle(axis, angle));
+
+    return this;
+  };
+})();
