37f544ea0ba5e8d6bdcdf012ac583436c0767e36
rohandeshpande <rohandeshpande@nbnco.com.au>
2018-11-22 11:14:54 +1100

    Ported math/ArraySpan to es6

--- src/math/ArraySpan.js
+++ src/math/ArraySpan.js
@@ -1,51 +1,48 @@
-(function(Proton, undefined) {
-    /**
-     * ArraySpan name get a random Color from a colors array
-     * @param {String|Array} colors - colors array
-     * @example 
-     * var span = new Proton.ArraySpan(["#fff","#ff0","#000"]);
-     * or
-     * var span = new Proton.ArraySpan("#ff0");
-     * @extends {Proton.Span}
-     * @constructor
-     */
+import MathUtils from './MathUtils';
+import Span from './Span';
+import Util from '../utils/Util';
 
-    function ArraySpan(colors) {
-        this._arr = Proton.Util.isArray(colors) ? colors : [colors];
-    }
+export default class ArraySpan extends Span {
+  /**
+   * ArraySpan name get a random Color from a colors array
+   * @param {String|Array} colors - colors array
+   * @example
+   * var span = new ArraySpan(["#fff","#ff0","#000"]);
+   * or
+   * var span = new ArraySpan("#ff0");
+   * @extends {Span}
+   * @constructor
+   */
 
-    Proton.Util.inherits(ArraySpan, Proton.Span);
+  constructor(colors) {
+    super();
 
-    /**
-     * getValue function
-     * @name get a random Color
-     * @return {string} a hex color
-     */
-    ArraySpan.prototype.getValue = function() {
-        var color = this._arr[(this._arr.length * Math.random()) >> 0];
-        
-        if (color == 'random' || color == 'Random')
-            return Proton.MathUtils.randomColor();
-        else
-            return color;
-    }
+    this._arr = Util.isArray(colors) ? colors : [colors];
+  }
 
-    /**
-     * Proton.createArraySpan function
-     * @name get a instance of Span
-     * @param {number} a min number
-     * @param {number} b max number
-     * @param {number} c center number
-     * @return {number} return a instance of Span
-     */
-    Proton.createArraySpan = function(arr) {
-        if (!arr) return null;
-        if (arr instanceof Proton.ArraySpan)
-            return arr;
-        else 
-            return new Proton.ArraySpan(arr);
-    }
+  /**
+   * getValue function
+   * @name get a random Color
+   * @return {string} a hex color
+   */
+  getValue() {
+    var color = this._arr[(this._arr.length * Math.random()) >> 0];
 
-    Proton.ArraySpan = ArraySpan;
+    if (color == 'random' || color == 'Random') return MathUtils.randomColor();
+    else return color;
+  }
+}
 
-})(Proton);
+/**
+ * createArraySpan function
+ * @name get a instance of Span
+ * @param {number} a min number
+ * @param {number} b max number
+ * @param {number} c center number
+ * @return {number} return a instance of Span
+ */
+export const createArraySpan = arr => {
+  if (!arr) return null;
+  if (arr instanceof ArraySpan) return arr;
+  else return new ArraySpan(arr);
+};
