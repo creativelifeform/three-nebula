6de03b75085ec92d4d561669b2f12f046161d0b1
rohandeshpande <rohandeshpande@nbnco.com.au>
2018-11-23 16:46:24 +1100

    Added entry file which exports all public classes, time to write some tests

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { Proton } from './core';
+
+export {
+  BoxZone,
+  LineZone,
+  MeshZone,
+  PointZone,
+  ScreenZone,
+  SphereZone
+} from './zone';
+
+export {
+  Body,
+  Life,
+  Mass,
+  Position,
+  Radius,
+  Rate,
+  Velocity
+} from './initialize';
+
+export {
+  Alpha,
+  Attraction,
+  Collision,
+  Color,
+  CrossZone,
+  Force,
+  Gravity,
+  RandomDrift,
+  Repulsion,
+  Rotate,
+  Scale,
+  Spring
+} from './behaviour';
+
+export { Particle, Pool } from './core';
+
+export default Proton;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
 /*global __dirname, require, module*/
 
-const path = require("path");
+const path = require('path');
 
-let libraryName = "three-proton";
+let libraryName = 'Proton';
 
 let plugins = [],
   outputFile;
 
-outputFile = libraryName + ".js";
+outputFile = 'three-proton.js';
 
 const config = {
-  entry: __dirname + "/src/index.js",
-  devtool: "source-map",
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
   output: {
-    path: __dirname + "/dist",
+    path: __dirname + '/build',
     filename: outputFile,
     library: libraryName,
-    libraryTarget: "umd",
+    libraryTarget: 'umd',
     umdNamedDefine: true
   },
   module: {
     rules: [
       {
         test: /(\.jsx|\.js)$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /(node_modules|bower_components)/
       },
       {
         test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    modules: [path.resolve("./src"), path.resolve("./node_modules")],
-    extensions: [".json", ".js"]
+    modules: [path.resolve('./src'), path.resolve('./node_modules')],
+    extensions: ['.json', '.js']
   },
   plugins: plugins
 };
