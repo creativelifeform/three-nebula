2700b0e76b4489bbd78c3093460787446007339c
rohandeshpande <rohandeshpande@nbnco.com.au>
2018-11-29 16:30:54 +1100

    Added esdoc json and api docs. Have a nice standard going now for documenting classes outlined in the Alpha and Attraction behaviours

--- src/behaviour/Alpha.js
+++ src/behaviour/Alpha.js
@@ -9,28 +9,42 @@ import Behaviour from './Behaviour';
 /**
  * Behaviour that applies an alpha transition effect to particles.
  *
- * @property {number} alphaA - the starting alpha value
- * @property {number} alphaB - the ending alpha value
- * @property {boolean} _same - determines if the values are the same
  */
 export default class Alpha extends Behaviour {
   /**
-   * @constructs {Alpha}
+   * Constructs an Alpha behaviour instance.
    *
-   * @param {number} alphaA - the starting alpha value
-   * @param {?number} alphaB - the ending alpha value
-   * @param {number} life - the life of the behaviour
-   * @param {function} easing - the easing equation to use for transforms
+   * @param {number} alphaA - The starting alpha value
+   * @param {?number} alphaB - The ending alpha value
+   * @param {number} life - The life of the behaviour
+   * @param {function} easing - The easing equation to use for transforms
    * @return void
    */
-  constructor(alphaA = 1, alphaB = null, life = Infinity, easing = defaultEasing) {
+  constructor(
+    alphaA = 1,
+    alphaB = null,
+    life = Infinity,
+    easing = defaultEasing
+  ) {
     super(life, easing);
+
+    /**
+     * @desc The starting alpha value
+     * @type {number|Span}
+     */
+    this.alphaA = alphaA;
+
+    /**
+     * @desc The ending alpha value
+     * @type {number|Span}
+     */
+    this.alphaB = alphaB;
 
     this.reset(alphaA, alphaB);
   }
 
   /**
-   * Gets the _same property.
+   * Gets the _same property which determines if the alpha are the same.
    *
    * @return {boolean}
    */
@@ -39,12 +53,15 @@ export default class Alpha extends Behaviour {
   }
 
   /**
-   * Sets the _same property.
+   * Sets the _same property which determines if the alpha are the same.
    *
    * @param {boolean} same
    * @return {boolean}
    */
   set same(same) {
+    /**
+     * @type {boolean}
+     */
     this._same = same;
   }
 
@@ -58,7 +75,7 @@ export default class Alpha extends Behaviour {
    * @return void
    */
   reset(alphaA = 1, alphaB = null, life = Infinity, easing = defaultEasing) {
-    this.same = (alphaB === null || alphaB === undefined) ? true : false;
+    this.same = alphaB === null || alphaB === undefined ? true : false;
     this.alphaA = createSpan(alphaA);
     this.alphaB = createSpan(alphaB);
 
--- src/behaviour/constants.js
+++ src/behaviour/constants.js
@@ -2,3 +2,6 @@ import { easeLinear } from '../ease';
 
 export const DEFAULT_BEHAVIOUR_EASING = easeLinear;
 export const PARTICLE_ALPHA_THRESHOLD = 0.002;
+export const DEFAULT_ATTRACTION_FORCE_SCALAR = 100;
+export const DEFAULT_ATTRACITON_RADIUS = 1000;
+export const PARTICLE_LENGTH_SQ_THRESHOLD = 0.000004;
