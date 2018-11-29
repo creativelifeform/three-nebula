cead3fd1513777ce1d58251491346d9e447a0510
Rohan Deshpande <rohan@creativelifeform.com>
2018-11-29 18:08:24 +1100

    Split behaviour tests into test module. Added Collision tests

--- test/behaviour/Alpha.spec.js
+++ test/behaviour/Alpha.spec.js
@@ -1,17 +1,16 @@
 /*global describe, it */
 
-import * as Proton from '../src';
+import * as Proton from '../../src';
 
-import { TIME } from './constants';
+import { TIME } from '../constants';
 import chai from 'chai';
 
 const { assert } = chai;
 
-describe('Alpha', () => {
+describe('behaviour -> Alpha', () => {
   const behaviour = new Proton.Alpha(1, 0);
 
   it('should instantiate with the correct properties', done => {
-    const behaviour = new Proton.Alpha(1, 0);
     const {
       life,
       age,
@@ -95,73 +94,6 @@ describe('Alpha', () => {
     assert.isFalse(alphaB._center);
     assert.strictEqual(alphaB.a, 0);
     assert.strictEqual(alphaB.b, 0);
-
-    done();
-  });
-});
-
-describe('Attraction', () => {
-  const behaviour = new Proton.Attraction();
-
-  it('should instantiate with the correct properties', done => {
-    const {
-      life,
-      easing,
-      age,
-      energy,
-      dead,
-      targetPosition,
-      radius,
-      force,
-      radiusSq,
-      attractionForce,
-      lengthSq
-    } = behaviour;
-
-    assert.strictEqual(life, Infinity);
-    assert.isFunction(easing);
-    assert.strictEqual(age, 0);
-    assert.strictEqual(energy, 1);
-    assert.isFalse(dead);
-    assert.isTrue(targetPosition instanceof Proton.Vector3D);
-    assert.strictEqual(radius, 1000);
-    assert.strictEqual(force, 10000);
-    assert.strictEqual(radiusSq, 1000000);
-    assert.isTrue(attractionForce instanceof Proton.Vector3D);
-    assert.strictEqual(lengthSq, 0);
-
-    done();
-  });
-
-  it('should have the correct properties after applying behaviour', done => {
-    const {
-      life,
-      easing,
-      age,
-      energy,
-      dead,
-      targetPosition,
-      radius,
-      force,
-      radiusSq,
-      attractionForce,
-      lengthSq
-    } = behaviour;
-    const particle = new Proton.Particle();
-
-    behaviour.applyBehaviour(particle, TIME);
-
-    assert.strictEqual(life, Infinity);
-    assert.isFunction(easing);
-    assert.strictEqual(age, 0);
-    assert.strictEqual(energy, 1);
-    assert.isFalse(dead);
-    assert.isTrue(targetPosition instanceof Proton.Vector3D);
-    assert.strictEqual(radius, 1000);
-    assert.strictEqual(force, 10000);
-    assert.strictEqual(radiusSq, 1000000);
-    assert.isTrue(attractionForce instanceof Proton.Vector3D);
-    assert.strictEqual(lengthSq, 0);
 
     done();
   });
