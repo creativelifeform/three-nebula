6627ec9dbb3442c5e3f40bc2d0022c3cc97427c7
Rohan Deshpande <rohan@creativelifeform.com>
2018-12-01 10:34:54 +1100

    Added unit tests for Mass property

---
+++ test/emitter/property/Mass.spec.js
@@ -0,0 +1,41 @@
+/*global describe, it */
+
+import * as Proton from '../../../src';
+
+import chai from 'chai';
+
+const { assert } = chai;
+
+describe('property -> Mass', () => {
+  const min = 3;
+  const max = 5;
+  const property = new Proton.Mass(min, max);
+
+  it('should have the correct properties after instantiation', done => {
+    const {
+      massPan,
+      massPan: { _isArray, a, b, _center }
+    } = property;
+
+    assert.instanceOf(massPan, Proton.Span);
+    assert.isFalse(_isArray);
+    assert.isFalse(_center);
+    assert.strictEqual(a, min);
+    assert.strictEqual(b, max);
+
+    done();
+  });
+
+  it('should set the correct properties on the particle after initialization', done => {
+    const particle = new Proton.Particle();
+
+    property.initialize(particle);
+
+    const { mass } = particle;
+
+    assert.isAbove(mass, min);
+    assert.isBelow(mass, max);
+
+    done();
+  });
+});
