9b1cb7e75caf64125416577bcdb2f168c7fd9818
Rohan Deshpande <rohan@creativelifeform.com>
2018-12-01 21:18:17 +1100

    Added doc blocks for InitializeUtil.initialize method. This needs some refactoring so I'll cover this

--- docs/js/three-proton.js
+++ docs/js/three-proton.js
@@ -51054,16 +51054,26 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var particleEuler = new _three.Euler();
 
 exports.default = {
-  initialize: function initialize(emitter, particle, initializes) {
-    var i = initializes.length;
+  /**
+   * Loops through the initializers array and calls each initializer's initialize method
+   * on the supplied particle. This sets the particle's initial properties.
+   *
+   * @see {@link '../emitter/Emitter'} setupParticle
+   * @param {Emitter} emitter - The emitter that has called this method
+   * @param {Particle} particle - The particle that has just been created
+   * @param {array<Initializer>} initializers - All of the emitter's initializers
+   * @return void
+   */
+  initialize: function initialize(emitter, particle, initializers) {
+    var i = initializers.length;
 
     while (i--) {
-      var initialize = initializes[i];
+      var initializer = initializers[i];
 
       // TODO remove this conditional, the else is not entered in any of the examples
       // and is just confusing
       /* istanbul ignore else */
-      if (initialize instanceof _Initializer2.default) initialize.init(emitter, particle);else this.init(emitter, particle, initialize);
+      if (initializer instanceof _Initializer2.default) initializer.init(emitter, particle);else this.init(emitter, particle, initializer);
     }
 
     this.bindEmitter(emitter, particle);
--- src/initialize/InitializeUtil.js
+++ src/initialize/InitializeUtil.js
@@ -5,17 +5,28 @@ import Util from '../utils/Util';
 const particleEuler = new Euler();
 
 export default {
-  initialize: function(emitter, particle, initializes) {
-    var i = initializes.length;
+  /**
+   * Loops through the initializers array and calls each initializer's initialize method
+   * on the supplied particle. This sets the particle's initial properties.
+   *
+   * @see {@link '../emitter/Emitter'} setupParticle
+   * @param {Emitter} emitter - The emitter that has called this method
+   * @param {Particle} particle - The particle that has just been created
+   * @param {array<Initializer>} initializers - All of the emitter's initializers
+   * @return void
+   */
+  initialize: function(emitter, particle, initializers) {
+    var i = initializers.length;
 
     while (i--) {
-      var initialize = initializes[i];
+      var initializer = initializers[i];
 
       // TODO remove this conditional, the else is not entered in any of the examples
       // and is just confusing
       /* istanbul ignore else */
-      if (initialize instanceof Initializer) initialize.init(emitter, particle);
-      else this.init(emitter, particle, initialize);
+      if (initializer instanceof Initializer)
+        initializer.init(emitter, particle);
+      else this.init(emitter, particle, initializer);
     }
 
     this.bindEmitter(emitter, particle);
