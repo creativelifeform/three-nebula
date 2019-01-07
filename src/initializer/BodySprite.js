01bd53aed2225e16fcb30062fee78317ff14b024
Rohan Deshpande <rohan@creativelifeform.com>
2019-01-08 07:47:26 +1100

    Updated the BodySprite class to accept an optional loadedTexture argument, the old from-json-promise branch can now be safely deleted

--- src/initializer/BodySprite.js
+++ src/initializer/BodySprite.js
@@ -2,55 +2,79 @@ import { Sprite, SpriteMaterial, TextureLoader } from 'three';
 
 import { DEFAULT_MATERIAL_PROPERTIES } from './constants';
 import Initializer from './Initializer';
-import { INITIALIZER_TYPE_BODY_SPRITE as type } from './types';
 
 /**
  * Sets the body property to be a THREE.Sprite on initialized particles.
  *
- * NOTE The texture map MUST be set on the SpriteMaterial in the TextureLoader.load
- * callback. Not doing so will cause WebGL buffer errors.
+ * NOTE The Texture MUST be loaded either prior to instantiation or within this class'
+ * constructor. Not doing so will cause WebGL buffer errors.
  */
 export default class BodySprite extends Initializer {
   /**
    * Constructs a BodySprite initializer.
    *
    * @param {string} texture - The sprite texture
-   * @param {object} materialProperties - The sprite material properties
-   * @throws {Error} If the TextureLoader fails to load the supplied texture
-   * @return void
+   * @param {object|undefined} materialProperties - The sprite material properties
+   * @param {?Texture} loadedTexture - Preloaded THREE.Texture instance
    */
-  constructor(texture, materialProperties = DEFAULT_MATERIAL_PROPERTIES) {
-    super(type);
+  constructor(
+    texture,
+    materialProperties = DEFAULT_MATERIAL_PROPERTIES,
+    loadedTexture = null
+  ) {
+    super();
 
-    new TextureLoader().load(
-      texture,
-      map => {
-        /**
-         * @desc The texture for the THREE.SpriteMaterial map.
-         * @type {Texture}
-         */
-        this.texture = map;
+    // Ensures that even if no materialProperties are passed, defaults are set
+    // This can happen if a loaded texture is provided
+    const matProps = materialProperties || DEFAULT_MATERIAL_PROPERTIES;
 
-        /**
-         * @desc THREE.SpriteMaterial instance.
-         * @type {SpriteMaterial}
-         */
-        this.material = new SpriteMaterial({
-          ...{ map },
-          ...materialProperties
-        });
+    if (loadedTexture) {
+      /**
+       * @desc The texture for the THREE.SpriteMaterial map.
+       * @type {Texture}
+       */
+      this.texture = loadedTexture;
 
-        /**
-         * @desc THREE.Sprite instance.
-         * @type {Sprite}
-         */
-        this.sprite = new Sprite(this.material);
-      },
-      undefined,
-      error => {
-        throw new Error(error);
-      }
-    );
+      /**
+       * @desc THREE.SpriteMaterial instance.
+       * @type {SpriteMaterial}
+       */
+      this.material = new SpriteMaterial({
+        ...{ map: loadedTexture },
+        ...matProps
+      });
+
+      /**
+       * @desc THREE.Sprite instance.
+       * @type {Sprite}
+       */
+      this.sprite = new Sprite(this.material);
+
+      return;
+    }
+
+    new TextureLoader().load(texture, map => {
+      /**
+       * @desc The texture for the THREE.SpriteMaterial map.
+       * @type {Texture}
+       */
+      this.texture = map;
+
+      /**
+       * @desc THREE.SpriteMaterial instance.
+       * @type {SpriteMaterial}
+       */
+      this.material = new SpriteMaterial({
+        ...{ map },
+        ...materialProperties
+      });
+
+      /**
+       * @desc THREE.Sprite instance.
+       * @type {Sprite}
+       */
+      this.sprite = new Sprite(this.material);
+    });
   }
 
   /**
@@ -69,11 +93,16 @@ export default class BodySprite extends Initializer {
    * @param {object} json - The JSON to construct the instance from.
    * @param {string} json.texture - The sprite texture
    * @param {object} json.materialProperties - The sprite material properties
+   * @param {?Texture} loadedTexture - Preloaded THREE.Texture instance
    * @return {BodySprite}
    */
   static fromJSON(json) {
-    const { texture, materialProperties = DEFAULT_MATERIAL_PROPERTIES } = json;
+    const {
+      texture,
+      materialProperties = DEFAULT_MATERIAL_PROPERTIES,
+      loadedTexture = null
+    } = json;
 
-    return new BodySprite(texture, materialProperties);
+    return new BodySprite(texture, materialProperties, loadedTexture);
   }
 }
