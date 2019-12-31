b968fac79f283792e333e49f8c2bedbe91fde967
Rohan Deshpande <rohan@creativelifeform.com>
2019-12-31 18:02:43 +1100

    Added titles for examples routes

--- website/content/routes.js
+++ website/content/routes.js
@@ -2,51 +2,59 @@
 
 module.exports = [
   {
-    title: 'Three Nebula - A Particle System Engine For Three Js',
+    title: 'Three Nebula - A Particle System Engine For ThreeJs',
     name: 'Home',
     path: '/',
   },
   {
-    title: 'Three Nebula - Particle System Examples',
+    title: 'Three Nebula - ThreeJs Particle System Engine Examples',
     name: 'Examples',
     path: '/examples',
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Custom Renderer',
     name: 'custom-renderer',
     path: '/examples/custom-renderer',
     shouldHideFromNav: true,
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Multiple Emitters',
     name: 'multiple-emitters',
     path: '/examples/multiple-emitters',
     shouldHideFromNav: true,
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Multiple Emitters',
     name: 'emitter-behaviors',
     path: '/examples/emitter-behaviors',
     shouldHideFromNav: true,
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Mesh Emitter',
     name: 'mesh-emitter',
     path: '/examples/mesh-emitter',
     shouldHideFromNav: true,
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Particle Collision',
     name: 'particle-collision',
     path: '/examples/particle-collision',
     shouldHideFromNav: true,
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Particle Gravity',
     name: 'gravity',
     path: '/examples/gravity',
     shouldHideFromNav: true,
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Point Zone',
     name: 'point-zone',
     path: '/examples/point-zone',
     shouldHideFromNav: true,
   },
   {
+    title: 'Three Nebula - ThreeJs Particle System Engine Snow',
     name: 'snow',
     path: '/examples/snow',
     shouldHideFromNav: true,
