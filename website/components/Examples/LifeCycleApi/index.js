9622952f4f43d7120fb71f16a36b322d1d8653a9
Rohan Deshpande <rohan@creativelifeform.com>
2020-01-10 07:17:53 +1100

    Added some padding to life cycle feedback emojis for better display in firefox and safari

--- website/components/Examples/LifeCycleApi/index.js
+++ website/components/Examples/LifeCycleApi/index.js
@@ -27,38 +27,56 @@ class Feedback extends Component {
       <ul className="LifeCycleApiFeedback">
         <li>
           {!hasStarted && (
-            <span role="img" aria-label="cross-mark">
-              ❌Started
-            </span>
+            <>
+              <span role="img" aria-label="cross-mark">
+                ❌
+              </span>
+              <span>Started</span>
+            </>
           )}
           {hasStarted && (
-            <span role="img" aria-label="check-mark-button">
-              ✅Started
-            </span>
+            <>
+              <span role="img" aria-label="check-mark-button">
+                ✅
+              </span>
+              <span>Started</span>
+            </>
           )}
         </li>
         <li>
           {isUpdating && (
-            <span role="img" aria-label="check-mark-button">
-              ✅Updating
-            </span>
+            <>
+              <span role="img" aria-label="check-mark-button">
+                ✅
+              </span>
+              <span>Updating</span>
+            </>
           )}
           {!isUpdating && (
-            <span role="img" aria-label="cross-mark">
-              ❌Updating
-            </span>
+            <>
+              <span role="img" aria-label="cross-mark">
+                ❌
+              </span>
+              <span>Updating</span>
+            </>
           )}
         </li>
         <li>
           {hasEnded && (
-            <span role="img" aria-label="check-mark-button">
-              ✅Ended
-            </span>
+            <>
+              <span role="img" aria-label="check-mark-button">
+                ✅
+              </span>
+              <span>Ended</span>
+            </>
           )}
           {!hasEnded && (
-            <span role="img" aria-label="cross-mark">
-              ❌Ended
-            </span>
+            <>
+              <span role="img" aria-label="cross-mark">
+                ❌
+              </span>
+              <span>Ended</span>
+            </>
           )}
         </li>
       </ul>
