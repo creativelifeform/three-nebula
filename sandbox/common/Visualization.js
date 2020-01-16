8e2194f3f02f030d49189185789ea574fc905a83
Rohan Deshpande <rohan@creativelifeform.com>
2020-01-16 18:21:17 +1100

    Added stats-js as dep to sandbox via unpkg cdn, also added the API integration in the vis class

--- sandbox/common/Visualization.js
+++ sandbox/common/Visualization.js
@@ -4,6 +4,8 @@ window.Visualization = class {
     this.init = init;
     this.shouldAnimate = true;
     this.shouldRotateCamera = shouldRotateCamera || true;
+    this.stats = window.Stats ? new window.Stats() : null;
+    this.container = document.getElementById('app');
   }
 
   /**
@@ -38,6 +40,9 @@ window.Visualization = class {
    */
   render() {
     window.addEventListener('resize', () => this.resize());
+
+    this.addStats();
+
     const animate = () => {
       if (!this.shouldAnimate) {
         return;
@@ -48,6 +53,7 @@ window.Visualization = class {
       this.particleSystem.update();
       this.rotateCamera();
       this.webGlRenderer.render(this.scene, this.camera);
+      this.updateStats();
     };
 
     animate();
@@ -67,81 +73,97 @@ window.Visualization = class {
     webGlRenderer.setSize(clientWidth, clientHeight, false);
   }
 
-
-  makeScene() {
-
-    //this.scene = new THREE.Scene();
-    
-    class BufferedScene extends THREE.Scene
-    {
-        constructor(){
-            super()
-            //this.sprites=[]
-            this.system = new ParticlePool({depthTest:false,depthWrite:false,transparent:true})
-            //this.system.points.material.wireframe = true;
-            
-            var sys = this.system;
-            var points = sys.points;
-            var ti=0;
-            var textureReferences={}
-            this.updateFn = (p)=>{
-                var sp=p.spr;
-                //sp.visible = false;
-                p.px= sp.position.x
-                p.py= sp.position.y + 300
-                p.pz= sp.position.z;
-                var c= sp.material.color;
-                p.cr=c.r;
-                p.cg=c.g;
-                p.cb=c.b;
-                var map = sp.material.map;
-                if(map._tileIndex==undefined){
-                    if(map.image && map.image.complete){
-                        var v = textureReferences[map.uuid]
-                        if(!v)v=textureReferences[map.uuid]=ti++
-                        map._tileIndex = v;
-                        p.t0=p.t1b=map._tileIndex;
-                    }
-                }else                 
-                    p.t0=p.t1b=map._tileIndex;
-                p.ca=sp.material.opacity;
-                p.s=sp.scale.x*2;
-                if(p.dead)
-                    return false;
-            }
-            this.system.points.onBeforeRender = (a,b,c,d,e,f)=>{
-                var rendering = true;
-                sys.transform(this.updateFn)
-            }
-            this.add(this.system.points)
-            //this.add(this.system.ribbons)
-        }
-        remove(e){
-            if(e.isSprite){
-                //var pos = this.sprites.indexOf(e)
-                //var p  = this.sprites.pop();
-                //if(pos<this.sprites.length)this.sprites[pos]=p;
-                e.userData.particle.dead = true;
-                //this.system.dealloc(this.system,e.userData.particle)
-            }
-            super.remove(e)
-        }
-        add(e){
-            if(e.isSprite){
-                //e.visible = false;
-                e.userData.particle = this.system.alloc((p)=>{
-                    p.spr=e;
-                    this.updateFn(p);
-                    //p.s = 100;
-                })
-                //this.sprites.push(e)
-            }            
-            super.add(e)
-        }
+  addStats() {
+    if (!this.stats) {
+      return;
     }
 
-    this.scene = new THREE.Scene()
-    this.scene = new BufferedScene()
+    this.container.appendChild(this.stats.dom);
+    this.stats.begin();
+  }
+
+  updateStats() {
+    if (!this.stats) {
+      return;
+    }
+
+    this.stats.end();
+  }
+
+  makeScene() {
+    //this.scene = new THREE.Scene();
+
+    class BufferedScene extends THREE.Scene {
+      constructor() {
+        super();
+        //this.sprites=[]
+        this.system = new ParticlePool({
+          depthTest: false,
+          depthWrite: false,
+          transparent: true,
+        });
+        //this.system.points.material.wireframe = true;
+
+        var sys = this.system;
+        var points = sys.points;
+        var ti = 0;
+        var textureReferences = {};
+        this.updateFn = p => {
+          var sp = p.spr;
+          //sp.visible = false;
+          p.px = sp.position.x;
+          p.py = sp.position.y + 300;
+          p.pz = sp.position.z;
+          var c = sp.material.color;
+          p.cr = c.r;
+          p.cg = c.g;
+          p.cb = c.b;
+          var map = sp.material.map;
+          if (map._tileIndex == undefined) {
+            if (map.image && map.image.complete) {
+              var v = textureReferences[map.uuid];
+              if (!v) v = textureReferences[map.uuid] = ti++;
+              map._tileIndex = v;
+              p.t0 = p.t1b = map._tileIndex;
+            }
+          } else p.t0 = p.t1b = map._tileIndex;
+          p.ca = sp.material.opacity;
+          p.s = sp.scale.x * 2;
+          if (p.dead) return false;
+        };
+        this.system.points.onBeforeRender = (a, b, c, d, e, f) => {
+          var rendering = true;
+          sys.transform(this.updateFn);
+        };
+        this.add(this.system.points);
+        //this.add(this.system.ribbons)
+      }
+      remove(e) {
+        if (e.isSprite) {
+          //var pos = this.sprites.indexOf(e)
+          //var p  = this.sprites.pop();
+          //if(pos<this.sprites.length)this.sprites[pos]=p;
+          e.userData.particle.dead = true;
+          //this.system.dealloc(this.system,e.userData.particle)
+        }
+        super.remove(e);
+      }
+      add(e) {
+        if (e.isSprite) {
+          //e.visible = false;
+          e.userData.particle = this.system.alloc(p => {
+            p.spr = e;
+            this.updateFn(p);
+            //p.s = 100;
+          });
+          //this.sprites.push(e)
+        }
+        super.add(e);
+      }
+    }
+
+    this.scene = new THREE.Scene();
+    this.scene = new BufferedScene();
 
     return this;
   }
