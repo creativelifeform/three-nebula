6fdfd2e978d277e7b83fde2123f6eedec0699c70
manthrax <manthrax@gmail.com>
2020-11-03 13:40:19 -0800

    Optimization and comments..
    Move buffer invalidation out of particle update..
    Increase size atten to match sprite renderer.

--- src/renderer/GPURenderer/shaders/vertexShader.js
+++ src/renderer/GPURenderer/shaders/vertexShader.js
@@ -1,9 +1,11 @@
-const SIZE_ATTENUATION_FACTOR = '300.0';
+const SIZE_ATTENUATION_FACTOR = '1600.0';
 
 export const vertexShader = () => {
   return `
 
     uniform sampler2D uTexture;                                     //GPU
+
+    //atlasIndex is a 256x1 float texture of tile rectangles as r=minx g=miny b=maxx a=maxy
     uniform sampler2D atlasIndex;                                   //GPU
 
 
@@ -26,6 +28,7 @@ export const vertexShader = () => {
       targetAlpha = alpha;
     
       tileID = texID;                                               //GPU
+      //get the tile rectangle from the atlasIndex texture..
       tileRect = texture2D(atlasIndex,vec2((texID+.5)/256.,.5));    //GPU
 
       gl_PointSize = size * (${SIZE_ATTENUATION_FACTOR} / -mvPosition.z);
