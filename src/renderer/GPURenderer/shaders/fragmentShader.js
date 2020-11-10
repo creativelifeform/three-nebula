90aee17b2da780814d4667d26e299f1900a0ee3d
Rohan Deshpande <rohan@creativelifeform.com>
2020-11-10 14:14:33 +1100

    Removed double spaces and extra line spaces from shaders

--- src/renderer/GPURenderer/shaders/fragmentShader.js
+++ src/renderer/GPURenderer/shaders/fragmentShader.js
@@ -2,21 +2,20 @@ export const fragmentShader = () => {
   return `
     uniform vec3 baseColor;
     uniform sampler2D uTexture;
-    uniform sampler2D atlasIndex;                                   //GPU
+    uniform sampler2D atlasIndex; //GPU
 
-    varying vec3    targetColor;
-    varying float   targetAlpha;
-    varying vec4    tileRect;                                       //GPU
-    varying float   tileID;                                         //GPU
+    varying vec3 targetColor;
+    varying float targetAlpha;
+    varying vec4 tileRect; //GPU
+    varying float tileID; //GPU
 
     void main() {
-
       gl_FragColor = vec4(baseColor * targetColor, targetAlpha);
 
       vec2 uv = gl_PointCoord;
-      uv = mix(tileRect.xy,tileRect.zw,gl_PointCoord);              //GPU
+      uv = mix(tileRect.xy, tileRect.zw, gl_PointCoord); //GPU
 
-      gl_FragColor = gl_FragColor * texture2D(uTexture, uv) ;
+      gl_FragColor = gl_FragColor * texture2D(uTexture, uv);
 
     }
 `;
--- src/renderer/GPURenderer/shaders/vertexShader.js
+++ src/renderer/GPURenderer/shaders/vertexShader.js
@@ -2,44 +2,30 @@ import { SIZE_ATTENUATION_FACTOR } from './constants';
 
 export const vertexShader = () => {
   return `
-
-    uniform sampler2D uTexture;                                     //GPU
-
+    uniform sampler2D uTexture; //GPU
     //atlasIndex is a 256x1 float texture of tile rectangles as r=minx g=miny b=maxx a=maxy
-    uniform sampler2D atlasIndex;                                   //GPU
-
+    uniform sampler2D atlasIndex; //GPU
 
     attribute float size;
     attribute vec3 color;
     attribute float alpha;
-
-    attribute float texID;                                          //GPU
+    attribute float texID; //GPU
 
     varying vec3 targetColor;
     varying float targetAlpha;
-
-    varying vec4  tileRect;                                         //GPU
-    varying float  tileID;                                          //GPU
+    varying vec4 tileRect; //GPU
+    varying float tileID; //GPU
 
     void main() {
-
       vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
       targetColor = color;
       targetAlpha = alpha;
 
-      tileID = texID;                                               //GPU
+      tileID = texID; //GPU
       //get the tile rectangle from the atlasIndex texture..
+      tileRect = texture2D(atlasIndex, vec2((tileID + 0.5) / 256.0, 0.5)); //GPU
 
-
-      tileRect = texture2D(atlasIndex,vec2((tileID+.5)/256.,.5));    //GPU
-
-//	vec2 scale;
-//	scale.x = length( vec3( modelMatrix[ 0 ].x, modelMatrix[ 0 ].y, modelMatrix[ 0 ].z ) );
-//	scale.y = length( vec3( modelMatrix[ 1 ].x, modelMatrix[ 1 ].y, modelMatrix[ 1 ].z ) );
-
-
-      gl_PointSize = ((size*${SIZE_ATTENUATION_FACTOR}) / -mvPosition.z); // (${SIZE_ATTENUATION_FACTOR} / -mvPosition.z);
-
+      gl_PointSize = ((size*${SIZE_ATTENUATION_FACTOR}) / -mvPosition.z);
       gl_Position = projectionMatrix * mvPosition;
     }
 `;
